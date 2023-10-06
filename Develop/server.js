75742267963e7b6629e4edd1d1a02f0b0d030ce5
DallasThompson <duhtennis1@gmail.com>
2023-10-06 13:17:55 -0800

    second commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const notesRouter = require("./backendRoutes/notes");
 
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
 
 app.use(express.json());
 
